52713ea677c5a7fa51f35eda8908a663cc75275e
Alex <hi@alex-co.de>
2025-09-22 13:14:29 +0000

    chore(sonicmic3): Update button mappings to allign better with other devices

--- src/dicta_sonicMic_3_device.ts
+++ src/dicta_sonicMic_3_device.ts
@@ -1,15 +1,15 @@
 import {ButtonEvent, DeviceType, DictationDeviceBase, ImplementationType} from './dictation_device_base';
 
 const BUTTON_MAPPINGS = new Map<ButtonEvent, number>([
-  [ButtonEvent.TAB_BACKWARD, 1 << 0],
+  [ButtonEvent.REWIND, 1 << 0],
   // returns "3" in reality, see getInputBitmask
-  [ButtonEvent.TAB_FORWARD, 1 << 1],
+  [ButtonEvent.FORWARD, 1 << 1],
   [ButtonEvent.PLAY, 1 << 2],
   [ButtonEvent.RECORD, 1 << 8],
   [ButtonEvent.INSTR, 1 << 9],
-  [ButtonEvent.CUSTOM_LEFT, 1 << 10],
+  [ButtonEvent.TAB_BACKWARD, 1 << 10],
   [ButtonEvent.F2_B, 1 << 11],
-  [ButtonEvent.CUSTOM_RIGHT, 1 << 12],
+  [ButtonEvent.TAB_FORWARD, 1 << 12],
   [ButtonEvent.F3_C, 1 << 13],
   [ButtonEvent.F1_A, 1 << 15],
   [ButtonEvent.TRANSCRIBE, 1 << 16],
@@ -34,10 +34,10 @@ export class SonicMic3Device extends DictationDeviceBase {
 
   protected getInputBitmask(data: DataView): number {
     const raw = data.getUint32(4, false);
-    // TAB_FORWARD returns value "3", which is "11" in binary.
+    // FORWARD returns value "3", which is "11" in binary.
     // This makes it incompatible for single-bit bitmasking
-    // So we return 2 (11) instead & map this in BUTTON_MAPPINGS
-    if (raw === 3) return 2;
+    // So we return 2 (10) instead & map this in BUTTON_MAPPINGS
+    if (raw === 3) return 1 << 1;
     return raw;
   }
 
